af1deff817cc13c69510f902b85fd60ca708a448
OP-Bright <rowanbrite@gmail.com>
2026-01-19 11:57:19 -0600

    rough out authentication based on guide. it is missing details.

---
+++ server/auth.ts
@@ -0,0 +1,47 @@
+import express from 'express';
+import passport from 'passport';
+import GoogleStrategy from 'passport-google-oidc';
+// import db from './database/dbFile.ts';
+
+passport.use(new GoogleStrategy({
+  clientID: process.env['GOOGLE_CLIENT_ID'],
+  clientSecret: process.env['GOOGLE_CLIENT_SECRET'],
+  callbackURL: '/oauth2/redirect/google',
+  scope: [ 'profile' ]
+}, function verify () {
+  //temp
+}));
+
+const router = express.Router();
+
+router.get('/login', function (req, res, next) {
+  res.render('login');
+});
+
+router.get('/login/federated/google', passport.authenticate('google'));
+
+router.get('/oauth2/redirect/google', passport.authenticate('google', {
+  successRedirect: '/',
+  failureRedirect: '/login'
+}));
+
+router.post('/logout', function(req, res, next) {
+  req.logout(function(err) {
+    if (err) { return next(err); }
+    res.redirect('/');
+  });
+});
+
+passport.serializeUser(function(user, cb) {
+  process.nextTick(function() {
+    cb(null, { id: user.id, username: user.username, name: user.name });
+  });
+});
+
+passport.deserializeUser(function(user, cb) {
+  process.nextTick(function() {
+    return cb(null, user);
+  });
+});
+
+export default router;
--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,12 @@
 import { fileURLToPath } from 'node:url';
 import { dirname, join } from 'node:path';
 import express from 'express';
-import {test} from './database/script.js'
+import session from 'express-session'; //we'll see
+import passport from 'passport';
+import router from './auth.js' //we'll see
 
-// import test from './router.js';
+import {test} from './database/script.js'
+// import something for connection?
 
 const app = express();
 const port = 8000;
@@ -13,6 +16,15 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.use(express.json());
 app.use(express.static(join(__dirname, '..', 'dist')));
+app.use('/', router);
+
+app.use(session({
+  secret: 'temp test',
+  resave: false,
+  saveUninitialized: false,
+  // store: new SQLiteStore({ db: 'sessions.db', dir: './var/db' })
+}));
+app.use(passport.authenticate('session'));
 
 app.listen(port, () => {
   console.info(`Listening on http://localhost:${port}`);
