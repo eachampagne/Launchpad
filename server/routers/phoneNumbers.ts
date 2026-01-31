815d05c99b425d9634a30134ebb1ed293650ce11
azaria <azariaplayer@gmail.com>
2026-01-31 14:58:48 -0600

    POST is working as expected

--- server/routers/phoneNumbers.ts
+++ server/routers/phoneNumbers.ts
@@ -20,4 +20,43 @@ phoneNumbers.get('/:ownerId', async (req, res) => {
   }
 })
 
+// POST - user CANNOT have two phone numbers
+phoneNumbers.post('/:ownerId', async (req, res) => {
+  const  ownerId  = Number(req.params.ownerId)
+  const {verified = false, notifications = false, contactNumber} = req.body as {verified:boolean, notifications:boolean, contactNumber:string}
+
+  // if no number is provided
+  if(!contactNumber){
+    res.status(404).send({error: 'You need to provide a phone number'})
+  }
+  try {
+    const existing = await prisma.phoneNumbers.findUnique({
+      where: {
+        userId: Number(req.params.ownerId)
+      }
+    })
+
+    // check if it exist
+    if(existing){
+      res.status(404).send('You already have a phone number')
+    }
+
+      await prisma.phoneNumbers.create({
+        data: {
+          userId: ownerId,
+          verified,
+          notifications,
+          contactNumber
+        }
+      })
+      res.status(201).send('Successful')
+    
+    
+
+  } catch (error) {
+    res.status(500).send({'Could not add the phone number': error})
+  }
+
+})
+
 export default phoneNumbers;
