a3e0c2fa60da69840a077354040af2d08615415f
Elizabeth Champagne <64558721+eachampagne@users.noreply.github.com>
2026-02-06 15:41:55 -0600

    GET /user exposes less information

--- server/routers/user.ts
+++ server/routers/user.ts
@@ -9,11 +9,21 @@ const user = express.Router();
 // GET
 
 // This is a temporary example function. It demonstrates how to 
-user.get('/', (req : any, res) => {
+user.get('/', (req, res) => {
   if (req.user) {
-    prisma.user.findUnique({where: {id: req.user.id}}).then((user: any) => { // ? Open Request: Update 'any' to accurately reflect what it is. It should be a User, but it freaks out when it is.
-      res.status(200).send(user);
-    })
+    try {
+      prisma.user.findUniqueOrThrow({where: {id: req.user.id}}).then((user) => { // ? Open Request: Update 'any' to accurately reflect what it is. It should be a User, but it freaks out when it is.        
+        // exposing the user's credential provider and subject seems bad
+        res.status(200).send({
+          id: user.id,
+          primaryDashId: user.primaryDashId,
+          name: user.name
+        });
+      })
+    } catch (error) {
+      console.error('Failed to find unique user in database despite user object existing on request:', error);
+      res.status(500);
+    }
   } else {
     res.status(200).send('You are not logged in.');
   }
